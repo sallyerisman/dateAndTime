bfd5152d9614773229318475c55b7f1c49394afe
Sally Erisman <sally.erisman@gmail.com>
2019-12-08 09:44:08 +0100

    Added weekday and seperated each date element by ID.

--- script.js
+++ script.js
@@ -3,11 +3,25 @@
 const clockEl = document.querySelector('#clock');
 const dateEl = document.querySelector('#date');
 const timeEl = document.querySelector('#time');
+const weekday = document.querySelector('#weekday');
+const dayOfMonth = document.querySelector('#dayOfMonth');
+const month = document.querySelector('#month');
+const year = document.querySelector('#year');
 
 const updateClock = () => {
-	dateEl.innerHTML = moment().format('YYYY-MM-DD');
-	timeEl.innerHTML = moment().format('HH:mm:ss');
+    // dateEl.innerHTML = moment().format('YYYY-MM-DD');
+    weekday.innerHTML = moment().format('dddd');
+    dayOfMonth.innerHTML = moment().format('D');
+    month.innerHTML = moment().format('MMM');
+    year.innerHTML = moment().format('YYYY');
+    
+
+
+    timeEl.innerHTML = moment().format('HH:mm:ss');
 }
+
 
 updateClock();
 const secondTicker = setInterval(updateClock, 1000);
+
+
